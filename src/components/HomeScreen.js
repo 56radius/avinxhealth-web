e3e4b57481177e4be330a3c6861cb7a038c8b165
56radius <cussmeritmohammed@gmail.com>
2023-12-20 01:24:51 +0100

    Done with the main part of the landing page, remaining the footer

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -181,6 +181,332 @@ function HomeScreen() {
           </div>
         </div>
       </section>
+
+      {/* the rest of the main link */}
+      <main id="main">
+        {/* About Section */}
+        <section id="about" className="about">
+          <div className="container">
+            <div className="row content">
+              <div className="col-lg-6">
+                <div className="col-lg-6 order-1 order-lg-2 hero-img">
+                  <img
+                    src={mind}
+                    style={{ width: "80%" }}
+                    className="img-fluid"
+                    alt=""
+                  />
+                </div>
+              </div>
+              <div className="col-lg-6 pt-4 pt-lg-0">
+                <p>
+                  Welcome to AVINX(Advanced Interracial Network Experience), a
+                  place where you can take the first step towards improving your
+                  health. But in this case we are focusing on your mental health
+                  with the help of artificial intelligence. Our mission is to
+                  make therapy accessible and affordable to everyone by using
+                  the latest AI technology.
+                </p>
+
+                <p className="fst-italic">
+                  Our AI therapists are programmed to assist you with a range of
+                  mental health concerns, including anxiety, depression, stress
+                  and relationship issues. At Avinx, we are committed to
+                  providing you with a personalized experience. Take the first
+                  step towards improving your mental health today by signing up
+                  for our AI therapy platform. Let our AI therapists help you
+                  navigate life's challenges and achieve your goals. The name of
+                  our AI therapists is AVA(Avinx Virtual Assistant).
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="why-us" className="why-us">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-4">
+                <div className="box">
+                  <span>01</span>
+                  <h4>Lorem Ipsum</h4>
+                  <p>
+                    Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et
+                    consectetur ducimus vero placeat
+                  </p>
+                </div>
+              </div>
+
+              <div className="col-lg-4 mt-4 mt-lg-0">
+                <div className="box">
+                  <span>02</span>
+                  <h4> Repellat Nihil </h4>
+                  <p>
+                    Dolorem est fugiat occaecati voluptate velit esse. Dicta
+                    veritatis dolor quod et vel dire leno para dest
+                  </p>
+                </div>
+              </div>
+
+              <div className="col-lg-4 mt-4 mt-lg-0">
+                <div className="box">
+                  <span>03</span>
+                  <h4> Ad ad velit qui</h4>
+                  <p>
+                    Molestiae officiis omnis illo asperiores. Aut doloribus
+                    vitae sunt debitis quo vel nam quis
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section id="faq" className="faq">
+          <div className="container">
+            <div className="section-title">
+              <h2>Frequently Asked Questions</h2>
+            </div>
+
+            <ul className="faq-list">
+              <li>
+                <div
+                  data-bs-toggle="collapse"
+                  className="collapsed question"
+                  href="#faq1"
+                >
+                  Non consectetur a erat nam at lectus urna duis?{" "}
+                  <i className="bi bi-chevron-down icon-show"></i>
+                  <i className="bi bi-chevron-up icon-close"></i>
+                </div>
+                <div id="faq1" className="collapse" data-bs-parent=".faq-list">
+                  <p>
+                    Feugiat pretium nibh ipsum consequat. Tempus iaculis urna id
+                    volutpat lacus laoreet non curabitur gravida. Venenatis
+                    lectus magna fringilla urna porttitor rhoncus dolor purus
+                    non.
+                  </p>
+                </div>
+              </li>
+
+              <li>
+                <div
+                  data-bs-toggle="collapse"
+                  href="#faq2"
+                  className="collapsed question"
+                >
+                  Feugiat scelerisque varius morbi enim nunc faucibus a
+                  pellentesque? <i className="bi bi-chevron-down icon-show"></i>
+                  <i className="bi bi-chevron-up icon-close"></i>
+                </div>
+                <div id="faq2" className="collapse" data-bs-parent=".faq-list">
+                  <p>
+                    Dolor sit amet consectetur adipiscing elit pellentesque
+                    habitant morbi. Id interdum velit laoreet id donec ultrices.
+                    Fringilla phasellus faucibus scelerisque eleifend donec
+                    pretium. Est pellentesque elit ullamcorper dignissim. Mauris
+                    ultrices eros in cursus turpis massa tincidunt dui.
+                  </p>
+                </div>
+              </li>
+
+              <li>
+                <div
+                  data-bs-toggle="collapse"
+                  href="#faq3"
+                  className="collapsed question"
+                >
+                  Dolor sit amet consectetur adipiscing elit pellentesque
+                  habitant morbi?{" "}
+                  <i className="bi bi-chevron-down icon-show"></i>
+                  <i className="bi bi-chevron-up icon-close"></i>
+                </div>
+                <div id="faq3" className="collapse" data-bs-parent=".faq-list">
+                  <p>
+                    Eleifend mi in nulla posuere sollicitudin aliquam ultrices
+                    sagittis orci. Faucibus pulvinar elementum integer enim. Sem
+                    nulla pharetra diam sit amet nisl suscipit. Rutrum tellus
+                    pellentesque eu tincidunt. Lectus urna duis convallis
+                    convallis tellus. Urna molestie at elementum eu facilisis
+                    sed odio morbi quis
+                  </p>
+                </div>
+              </li>
+
+              <li>
+                <div
+                  data-bs-toggle="collapse"
+                  href="#faq4"
+                  className="collapsed question"
+                >
+                  Ac odio tempor orci dapibus. Aliquam eleifend mi in nulla?{" "}
+                  <i className="bi bi-chevron-down icon-show"></i>
+                  <i className="bi bi-chevron-up icon-close"></i>
+                </div>
+                <div id="faq4" className="collapse" data-bs-parent=".faq-list">
+                  <p>
+                    Dolor sit amet consectetur adipiscing elit pellentesque
+                    habitant morbi. Id interdum velit laoreet id donec ultrices.
+                    Fringilla phasellus faucibus scelerisque eleifend donec
+                    pretium. Est pellentesque elit ullamcorper dignissim. Mauris
+                    ultrices eros in cursus turpis massa tincidunt dui.
+                  </p>
+                </div>
+              </li>
+
+              <li>
+                <div
+                  data-bs-toggle="collapse"
+                  href="#faq5"
+                  className="collapsed question"
+                >
+                  Tempus quam pellentesque nec nam aliquam sem et tortor
+                  consequat? <i className="bi bi-chevron-down icon-show"></i>
+                  <i className="bi bi-chevron-up icon-close"></i>
+                </div>
+                <div id="faq5" className="collapse" data-bs-parent=".faq-list">
+                  <p>
+                    Molestie a iaculis at erat pellentesque adipiscing commodo.
+                    Dignissim suspendisse in est ante in. Nunc vel risus commodo
+                    viverra maecenas accumsan. Sit amet nisl suscipit adipiscing
+                    bibendum est. Purus gravida quis blandit turpis cursus in
+                  </p>
+                </div>
+              </li>
+
+              <li>
+                <div
+                  data-bs-toggle="collapse"
+                  href="#faq6"
+                  className="collapsed question"
+                >
+                  Tortor vitae purus faucibus ornare. Varius vel pharetra vel
+                  turpis nunc eget lorem dolor?{" "}
+                  <i className="bi bi-chevron-down icon-show"></i>
+                  <i className="bi bi-chevron-up icon-close"></i>
+                </div>
+                <div id="faq6" className="collapse" data-bs-parent=".faq-list">
+                  <p>
+                    Laoreet sit amet cursus sit amet dictum sit amet justo.
+                    Mauris vitae ultricies leo integer malesuada nunc vel.
+                    Tincidunt eget nullam non nisi est sit amet. Turpis nunc
+                    eget lorem dolor sed. Ut venenatis tellus in metus vulputate
+                    eu scelerisque. Pellentesque diam volutpat commodo sed
+                    egestas egestas fringilla phasellus faucibus. Nibh tellus
+                    molestie nunc non blandit massa enim nec.
+                  </p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section>
+
+        <section id="contact" className="contact section-bg">
+          <div className="container">
+            <div className="section-title">
+              <h2>Contact</h2>
+              <p>
+                Magnam dolores commodi suscipit. Necessitatibus eius consequatur
+                ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam
+                quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.
+                Quia fugiat sit in iste officiis commodi quidem hic quas.
+              </p>
+            </div>
+
+            <div className="row">
+              <div className="col-lg-6">
+                <div className="row">
+                  <div className="col-md-12">
+                    <div className="info-box">
+                      <i className="bx bx-map"></i>
+                      <h3>Our Address</h3>
+                      <p>A108 Adam Street, New York, NY 535022</p>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="info-box mt-4">
+                      <i className="bx bx-envelope"></i>
+                      <h3>Email Us</h3>
+                      <p>
+                        info@example.com
+                        <br />
+                        contact@example.com
+                      </p>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="info-box mt-4">
+                      <i className="bx bx-phone-call"></i>
+                      <h3>Call Us</h3>
+                      <p>
+                        +1 5589 55488 55
+                        <br />
+                        +1 6678 254445 41
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-lg-6 mt-4 mt-lg-0">
+                <form className="php-email-form">
+                  <div className="row">
+                    <div className="col-md-6 form-group">
+                      <input
+                        type="text"
+                        name="name"
+                        className="form-control"
+                        id="name"
+                        placeholder="Your Name"
+                        required
+                      />
+                    </div>
+                    <div className="col-md-6 form-group mt-3 mt-md-0">
+                      <input
+                        type="email"
+                        className="form-control"
+                        name="email"
+                        id="email"
+                        placeholder="Your Email"
+                        required
+                      />
+                    </div>
+                  </div>
+                  <div className="form-group mt-3">
+                    <input
+                      type="text"
+                      className="form-control"
+                      name="subject"
+                      id="subject"
+                      placeholder="Subject"
+                      required
+                    />
+                  </div>
+                  <div className="form-group mt-3">
+                    <textarea
+                      className="form-control"
+                      name="message"
+                      rows="5"
+                      placeholder="Message"
+                      required
+                    ></textarea>
+                  </div>
+                  <div className="my-3">
+                    <div className="loading">Loading</div>
+                    <div className="error-message"></div>
+                    <div className="sent-message">
+                      Your message has been sent. Thank you!
+                    </div>
+                  </div>
+                  <div className="text-center">
+                    <button type="submit">Send Message</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
