1e3baab5474055db2773811f07c0a1ca40c9c414
56radius <cussmeritmohammed@gmail.com>
2023-12-27 22:02:22 +0100

    Designed the style for the chatbot interaction

--- src/components/dashboard/TryAvaScreen.js
+++ src/components/dashboard/TryAvaScreen.js
@@ -1,9 +1,23 @@
 import React from "react";
 
+import { useNavigate } from "react-router-dom";
+
+import "../.././assets/dashboard/assets/css/try_ava.css";
+
 function TryAvaScreen() {
+  const navigate = useNavigate();
   return (
     <div>
-      <p> Hello </p>
+      <h1>
+        <button
+          style={{ borderWidth: 0, backgroundColor: "white" }}
+          onClick={() => navigate("/dashboard")}
+        >
+          AVA(Avinx Virtual Assistant)
+        </button>{" "}
+      </h1>
+      <div id="chat-messages"></div>
+      <textarea id="message-input"></textarea>
     </div>
   );
 }
